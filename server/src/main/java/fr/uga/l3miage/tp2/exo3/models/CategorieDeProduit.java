f01a651095234067f48b06fda3c9014360094919
l3miage-kaddourm <mohammed.kaddour@etu.univ-grenoble-alpes.fr>
2024-03-18 15:07:22 +0100

    Fin Exo 3 Tp2 premiere version Binome: Kaddour Mohammed, Sbartai Achraf

--- server/src/main/java/fr/uga/l3miage/tp2/exo3/models/CategorieDeProduit.java
+++ server/src/main/java/fr/uga/l3miage/tp2/exo3/models/CategorieDeProduit.java
@@ -3,6 +3,7 @@ package fr.uga.l3miage.tp2.exo3.models;
 import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class CategorieDeProduit {
     @Id
     private Long id;
--- server/src/main/java/fr/uga/l3miage/tp2/exo3/models/Produit.java
+++ server/src/main/java/fr/uga/l3miage/tp2/exo3/models/Produit.java
@@ -2,6 +2,7 @@ package fr.uga.l3miage.tp2.exo3.models;
 
 import javax.persistence.*;
 
+@Entity
 public class Produit {
     @Id
     private Long id;
